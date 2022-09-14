81f22f916efbc7781916c8df74d13e1971380dfd
hima-94 <himasri8143@gmail.com>
2022-09-14 10:45:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+
+  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="control" >
+      <div class="container-fluid mt-3">
+      <div class="row">
+    <div class="col-sm-4 p-3 bg-primary text-white"><img src="https://crop-circle.imageonline.co/image.png" class="rounded-circle" alt="Cinque Terre" width="280" height="200"/> 
+    <p><h1>Akhil Chakravarthy</h1><br/>
+      CEO/Managing Director<br/>
+     <h1> TECHNO SOFTWARE SOLUTIONS</h1> </p></div> <br/>
+    <div class="col-sm-8 p-3 ">
+  
+   <div class="container mt-3  text-white">
+  <div class="mt-4 con   rounded">
+    <h4>About</h4> 
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat..</p> 
+  </div>
+</div>
     </div>
+  </div>
+</div>
+ </div>
+         
+     
+
+
+     
+     
+
+     
+
+  
+        
   );
 }
 
